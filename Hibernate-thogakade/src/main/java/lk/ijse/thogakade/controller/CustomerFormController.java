e57eb26579fcb7e673c2187154576f3309fa6e99
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-08-06 14:14:13 +0530

    Updated CustomerFormController save Method

--- Hibernate-thogakade/src/main/java/lk/ijse/thogakade/controller/CustomerFormController.java
+++ Hibernate-thogakade/src/main/java/lk/ijse/thogakade/controller/CustomerFormController.java
@@ -1,30 +1,24 @@
 package lk.ijse.thogakade.controller;
 
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import lk.ijse.thogakade.dto.Customer;
 import lk.ijse.thogakade.dto.tm.CustomerTM;
-import lk.ijse.thogakade.Repository.CustomerModel;
+import lk.ijse.thogakade.entity.Customer;
+import lk.ijse.thogakade.repository.CustomerRepository;
 
 import java.io.IOException;
-import java.sql.*;
-import java.util.List;
+import java.sql.SQLException;
 import java.util.Properties;
 import java.util.ResourceBundle;
+
+//import lk.ijse.thogakade.dto.Customer;
+//import lk.ijse.thogakade.Repository.CustomerModel;
 
 public class CustomerFormController implements Initializable {
     private static final String URL = "jdbc:mysql://localhost:3306/Thogakade";
@@ -70,45 +64,45 @@ public class CustomerFormController implements Initializable {
 
     @Override
     public void initialize(java.net.URL url, ResourceBundle resourceBundle) {
-        setCellValueFactory();
-        getAll();
+//        setCellValueFactory();
+//        getAll();
     }
 
-    private void setCellValueFactory() {
-        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
-        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
-    }
-
-    private void getAll() {
-        try {
-            ObservableList<CustomerTM> obList = FXCollections.observableArrayList();
-            List<Customer> cusList = CustomerModel.getAll();
-
-            for (Customer customer : cusList) {
-                obList.add(new CustomerTM(
-                        customer.getId(),
-                        customer.getName(),
-                        customer.getAddress(),
-                        customer.getSalary()
-                ));
-            }
-            tblCustomer.setItems(obList);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR, "SQL Error!").show();
-        }
-    }
+//    private void setCellValueFactory() {
+//        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+//        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+//        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+//        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+//    }
+//
+//    private void getAll() {
+//        try {
+//            ObservableList<CustomerTM> obList = FXCollections.observableArrayList();
+//            List<Customer> cusList = CustomerModel.getAll();
+//
+//            for (Customer customer : cusList) {
+//                obList.add(new CustomerTM(
+//                        customer.getId(),
+//                        customer.getName(),
+//                        customer.getAddress(),
+//                        customer.getSalary()
+//                ));
+//            }
+//            tblCustomer.setItems(obList);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            new Alert(Alert.AlertType.ERROR, "SQL Error!").show();
+//        }
+//    }
 
     @FXML
     void btnBackOnAction(ActionEvent event) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"));
-
-        Stage stage = (Stage) root.getScene().getWindow();
-        stage.setTitle("Dashboard");
-        stage.setScene(new Scene(parent));
-        stage.centerOnScreen();
+//        Parent parent = FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"));
+//
+//        Stage stage = (Stage) root.getScene().getWindow();
+//        stage.setTitle("Dashboard");
+//        stage.setScene(new Scene(parent));
+//        stage.centerOnScreen();
     }
 
     @FXML
@@ -118,88 +112,103 @@ public class CustomerFormController implements Initializable {
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) throws SQLException {
-        String id = txtId.getText();
-        try (Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "DELETE FROM Customer WHERE id = ?";
-
-            PreparedStatement pstm = con.prepareStatement(sql);
-            pstm.setString(1, id);
-
-            if (pstm.executeUpdate() > 0) {
-                new Alert(Alert.AlertType.CONFIRMATION, "deletd!").show();
-            }
-        }
+//        String id = txtId.getText();
+//        try (Connection con = DriverManager.getConnection(URL, props)) {
+//            String sql = "DELETE FROM Customer WHERE id = ?";
+//
+//            PreparedStatement pstm = con.prepareStatement(sql);
+//            pstm.setString(1, id);
+//
+//            if (pstm.executeUpdate() > 0) {
+//                new Alert(Alert.AlertType.CONFIRMATION, "deletd!").show();
+//            }
+//        }
     }
 
     @FXML
     void btnSaveOnAction(ActionEvent event) throws SQLException {
-        String id = txtId.getText();
-        String name = txtName.getText();
-        String address = txtAddress.getText();
-        double salary = Double.parseDouble(txtSalary.getText());
+        Customer customer = getCustomer();
 
-        try (Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "INSERT INTO Customer(id, name, address, salary)" +
-                    "VALUES(?, ?, ?, ?)";
-            PreparedStatement pstm = con.prepareStatement(sql);
-            pstm.setString(1, id);
-            pstm.setString(2, name);
-            pstm.setString(3, address);
-            pstm.setDouble(4, salary);
+        // 1. Save
+        CustomerRepository cusRepository = new CustomerRepository();
+        int savedCusId = cusRepository.saveCustomer(customer);
+        System.out.println("Saved Cus Id: " + savedCusId);
+//        String id = txtId.getText();
+//        String name = txtName.getText();
+//        String address = txtAddress.getText();
+//        double salary = Double.parseDouble(txtSalary.getText());
+//
+//        try (Connection con = DriverManager.getConnection(URL, props)) {
+//            String sql = "INSERT INTO Customer(id, name, address, salary)" +
+//                    "VALUES(?, ?, ?, ?)";
+//            PreparedStatement pstm = con.prepareStatement(sql);
+//            pstm.setString(1, id);
+//            pstm.setString(2, name);
+//            pstm.setString(3, address);
+//            pstm.setDouble(4, salary);
+//
+//            int affectedRows = pstm.executeUpdate();
+//            if (affectedRows > 0) {
+//                new Alert(Alert.AlertType.CONFIRMATION,
+//                        "huree!! customer added :)")
+//                        .show();
+//            }
+//        }
 
-            int affectedRows = pstm.executeUpdate();
-            if (affectedRows > 0) {
-                new Alert(Alert.AlertType.CONFIRMATION,
-                        "huree!! customer added :)")
-                        .show();
-            }
-        }
+    }
+
+    private Customer getCustomer() {
+        Customer customer = new Customer();
+        customer.setName(txtName.getText());
+        customer.setAddress(txtAddress.getText());
+        customer.setSalary(Double.valueOf(txtSalary.getText()));
+        return customer;
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) throws SQLException {
-        String id = txtId.getText();
-        String name = txtName.getText();
-        String address = txtAddress.getText();
-        double salary = Double.parseDouble(txtSalary.getText());
-
-        try (Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "UPDATE Customer SET name = ?, address = ?, salary = ? WHERE id = ?";
-            PreparedStatement pstm = con.prepareStatement(sql);
-            pstm.setString(1, name);
-            pstm.setString(2, address);
-            pstm.setDouble(3, salary);
-            pstm.setString(4, id);
-
-            boolean isUpdated = pstm.executeUpdate() > 0;
-            if (isUpdated) {
-                new Alert(Alert.AlertType.CONFIRMATION, "yes! updated!!").show();
-            }
-        }
+//        String id = txtId.getText();
+//        String name = txtName.getText();
+//        String address = txtAddress.getText();
+//        double salary = Double.parseDouble(txtSalary.getText());
+//
+//        try (Connection con = DriverManager.getConnection(URL, props)) {
+//            String sql = "UPDATE Customer SET name = ?, address = ?, salary = ? WHERE id = ?";
+//            PreparedStatement pstm = con.prepareStatement(sql);
+//            pstm.setString(1, name);
+//            pstm.setString(2, address);
+//            pstm.setDouble(3, salary);
+//            pstm.setString(4, id);
+//
+//            boolean isUpdated = pstm.executeUpdate() > 0;
+//            if (isUpdated) {
+//                new Alert(Alert.AlertType.CONFIRMATION, "yes! updated!!").show();
+//            }
+//        }
     }
 
     @FXML
     void codeSearchOnAction(ActionEvent event) throws SQLException {
-        String id = txtId.getText();
-
-        try (Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "SELECT * FROM Customer WHERE id = ?";
-            PreparedStatement pstm = con.prepareStatement(sql);
-            pstm.setString(1, id);
-
-            ResultSet resultSet = pstm.executeQuery();
-            if (resultSet.next()) {
-                String cus_id = resultSet.getString(1);
-                String cus_name = resultSet.getString(2);
-                String cus_address = resultSet.getString(3);
-                double cus_salary = resultSet.getDouble(4);
-
-                txtId.setText(cus_id);
-                txtName.setText(cus_name);
-                txtAddress.setText(cus_address);
-                txtSalary.setText(String.valueOf(cus_salary));
-            }
-        }
+//        String id = txtId.getText();
+//
+//        try (Connection con = DriverManager.getConnection(URL, props)) {
+//            String sql = "SELECT * FROM Customer WHERE id = ?";
+//            PreparedStatement pstm = con.prepareStatement(sql);
+//            pstm.setString(1, id);
+//
+//            ResultSet resultSet = pstm.executeQuery();
+//            if (resultSet.next()) {
+//                String cus_id = resultSet.getString(1);
+//                String cus_name = resultSet.getString(2);
+//                String cus_address = resultSet.getString(3);
+//                double cus_salary = resultSet.getDouble(4);
+//
+//                txtId.setText(cus_id);
+//                txtName.setText(cus_name);
+//                txtAddress.setText(cus_address);
+//                txtSalary.setText(String.valueOf(cus_salary));
+//            }
+//        }
     }
 
 }

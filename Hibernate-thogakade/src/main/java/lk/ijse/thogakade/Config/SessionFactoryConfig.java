d8ef072475e7a6a912638c3142d0c02246899f09
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-08-06 13:10:41 +0530

    Updated SessionFactoryConfig

--- Hibernate-thogakade/src/main/java/lk/ijse/thogakade/Config/SessionFactoryConfig.java
+++ Hibernate-thogakade/src/main/java/lk/ijse/thogakade/Config/SessionFactoryConfig.java
@@ -1,4 +1,34 @@
 package lk.ijse.thogakade.Config;
 
+import lk.ijse.thogakade.dto.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class SessionFactoryConfig {
+
+    private static  SessionFactoryConfig factoryConfig;
+    private final SessionFactory sessionFactory;
+
+    private SessionFactoryConfig(){
+//         sessionFactory  =new MetadataSources(new StandardServiceRegistryBuilder().
+//                 configure().build())
+//                 .addAnnotatedClass(Customer.class)
+//                 .getMetadataBuilder()
+//                 .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+//                 .build().buildSessionFactory();
+
+        sessionFactory = new Configuration().configure().addAnnotatedClass(Customer.class).buildSessionFactory();
+    }
+
+
+    public static SessionFactoryConfig getInstance(){
+        return (null==factoryConfig)
+                ?factoryConfig=new SessionFactoryConfig():factoryConfig;
+
+    }
+    public Session getSession(){
+
+        return sessionFactory.openSession();
+    }
 }

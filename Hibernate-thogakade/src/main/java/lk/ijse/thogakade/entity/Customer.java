deb7cb3042d06da7e4c9ee233b997e24527c106e
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-08-06 13:32:59 +0530

    Updated Entity Customer

---
+++ Hibernate-thogakade/src/main/java/lk/ijse/thogakade/entity/Customer.java
@@ -0,0 +1,68 @@
+package lk.ijse.thogakade.entity;
+
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private int id;
+    @Column(name = "cusomer_name")
+    private String name;
+    @Column(name = "customer_address")
+    private String address;
+    @Column(name ="customer_salary")
+    private Double salary;
+    @CreationTimestamp
+    private Timestamp createdDateTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Customer() {
+    }
+
+
+
+    public Customer(int id, String name, String address, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+}
